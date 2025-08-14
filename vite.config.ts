855445e95216ef001d375c7b41c088fd08090684
T24085 <christoffersent@gmail.com>
2025-08-14 14:40:06 -0500

    Fix Vite config + TS import for GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: { "@": path.resolve(__dirname, "src") } },
-  base: "/christian-ministry/", // trailing slash matters
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  base: "/christian-ministry/",
 });
